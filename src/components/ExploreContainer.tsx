c334efb544de50d7d087ed10d1cc458fb0fd8670
Wilfred Lopez <cubamc@gmail.com>
2020-03-21 15:40:54 -0400

    init

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import './ExploreContainer.css';
+import React from "react"
+import "./ExploreContainer.css"
+import { IonText } from "@ionic/react"
 
 interface ContainerProps {
-  name: string;
+  name: string
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div className="container">
       <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
-};
+      <div>
+        <IonText>Created By Wilfred Lopez.</IonText>
+      </div>
+      <hr />
+      <div>
+        <p>Credits: </p>
 
-export default ExploreContainer;
+        <p>https://github.com/javieraviles/covidAPI</p>
+        <p>Javier Aviles, Ionic Framework, ReactJS</p>
+      </div>
+    </div>
+  )
+}
+
+export default ExploreContainer
