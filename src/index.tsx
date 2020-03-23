39632c59dedd28fb1f31943d70bc02083b83bcda
Wilfred Lopez <cubamc@gmail.com>
2020-03-23 00:47:56 -0400

    removed service workers

--- src/index.tsx
+++ src/index.tsx
@@ -8,18 +8,18 @@ ReactDOM.render(<App />, document.getElementById("root"))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-serviceWorker.register({
-  onUpdate: async registration => {
-    // We want to run this code only if we detect a new service worker is
-    // waiting to be activated.
-    // Details about it: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
-    if (registration && registration.waiting) {
-      console.log("Unregistering Service Worker")
-      await registration.unregister()
-      // Once the service worker is unregistered, we can reload the page to let
-      // the browser download a fresh copy of our app (invalidating the cache)
-      window.location.reload()
-    }
-  },
-})
+serviceWorker.unregister()
+// serviceWorker.register({
+//   onUpdate: async registration => {
+//     // We want to run this code only if we detect a new service worker is
+//     // waiting to be activated.
+//     // Details about it: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+//     if (registration && registration.waiting) {
+//       console.log("Unregistering Service Worker")
+//       await registration.unregister()
+//       // Once the service worker is unregistered, we can reload the page to let
+//       // the browser download a fresh copy of our app (invalidating the cache)
+//       window.location.reload()
+//     }
+//   },
+// })
